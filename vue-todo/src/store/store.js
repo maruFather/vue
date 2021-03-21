2d9796cced281b40532a522c11c0888bea3981e0
ggon45@gmail.com <ggon45@gmail.com>
2021-03-21 19:17:27 +0900

    Vuex 설치 및 등록

---
+++ vue-todo/src/store/store.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// Vue 를 사용하는 모든 영역에 사용할때 Vue.use()
+// EX) App.vue -> this.$store 로 사용가능.
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    // 
+});
