641bcca18b5cf4835dd2c58b0ad74406ba125b0b
ggon45@gmail.com <ggon45@gmail.com>
2021-03-21 19:31:53 +0900

    state 적용

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -5,6 +5,23 @@ import Vuex from 'vuex'
 // EX) App.vue -> this.$store 로 사용가능.
 Vue.use(Vuex);
 
+const storage = {
+    fetch() {
+        // app 이 실행된후 바로 실행 vue 라이프사이클 
+        const arr= [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {                    
+                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        }
+        return arr;
+    }
+}
+
 export const store = new Vuex.Store({
-    // 
+    state: {
+       todoItems: storage.fetch() 
+    }
 });
